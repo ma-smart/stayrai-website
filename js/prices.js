59d82787bec33da50a0930ac78a02deec1c90559
mr-ma <mohsen.ahmadv@gmail.com>
2022-01-29 14:12:48 +0100

    Update prices.js

--- js/prices.js
+++ js/prices.js
@@ -1,5 +1,5 @@
 const countryPrices = {
     "EU": {"rein_before": "€480", "rein_after": "€399", "einhorn_before": "€718", "einhorn_after": "€599", "boese_before": "€1200", "boese_after": "€999"},
-    "UK": {"rein_before": "£455", "rein_after": "£399", "einhorn_before": "£600", "einhorn_after": "£499", "boese_before": "£1005", "boese_after": "£899"},
-    "US": {"rein_before": "$540", "rein_after": "$399", "einhorn_before": "$810", "einhorn_after": "$599", "boese_before": "$1355", "boese_after": "$999"}
+    "UK": {"rein_before": "£480", "rein_after": "£399", "einhorn_before": "£718", "einhorn_after": "£599", "boese_before": "£1200", "boese_after": "£999"},
+    "US": {"rein_before": "$480", "rein_after": "$399", "einhorn_before": "$718", "einhorn_after": "$599", "boese_before": "$1200", "boese_after": "$999"}
 }
